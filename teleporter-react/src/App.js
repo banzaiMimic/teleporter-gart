bfb7ea34acc58aaf11043fd5ff081693901dcbec
devstackio <gartflores@gmail.com>
2019-11-15 15:36:18 -0500

    added basic graph using mermaid.io

--- teleporter-react/src/App.js
+++ teleporter-react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import CityGraph from './components/CityGraph'
 
 function App() {
   return (
     <>
       <p>teleporter-demo</p>
+      <CityGraph />
     </>
   )
 }
---
+++ teleporter-react/src/components/CityGraph.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import mermaid from 'mermaid'
+
+mermaid.initialize({
+  startOnLoad: true
+})
+
+class GraphDisplay extends React.Component {
+  componentDidMount() {
+    mermaid.contentLoaded();
+  }
+  render() {
+    return <div className="mermaid">{this.props.chart}</div>;
+  }
+}
+
+export default function CityGraph() {
+  return (
+    <GraphDisplay
+        chart={`graph LR;
+A[Fortuna]-->B[Hemingway];
+A-->C[Atlantis];
+B-->D[Chesterfield];
+D-->E[Springton];
+F[Los Amigos]-->G[Paristown];
+G-->H[Oaktown];
+F-->H;
+I[Summerton]-->E;
+I-->B;
+
+      `}
+      />
+  )
+}
