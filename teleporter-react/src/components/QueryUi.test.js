c3792cc46287404728da2d0f697a00f0aec263cd
devstackio <gartflores@gmail.com>
2019-11-17 12:02:10 -0500

    added react-test-renderer snapshot tests for QueryUi

---
+++ teleporter-react/src/components/QueryUi.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import QueryUi from './QueryUi'
+
+test('Load Default Input, Submit, Clear functionality snapshot tests', () => {
+  const component = renderer.create(<QueryUi />)
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
+  // trigger loadDefaultInput
+  const e = { preventDefault: jest.fn() }
+  const btnLoadDefaultInput = component.root.findAllByType('input')[2].props
+  const btnSubmit = component.root.findAllByType('input')[0].props
+  const btnClear = component.root.findAllByType('input')[1].props
+  
+  btnLoadDefaultInput.onClick(e)
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
+  // trigger submit
+  btnSubmit.onClick(e)
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
+  // trigger clear
+  btnClear.onClick(e)
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
+
+})
