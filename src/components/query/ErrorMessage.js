590cd71936191559194a7392834f5fdc5b91aeee
devstackio <gartflores@gmail.com>
2019-11-17 13:55:38 -0500

    updating error message on input error

--- src/components/query/ErrorMessage.js
+++ src/components/query/ErrorMessage.js
@@ -4,10 +4,10 @@ export default function ErrorMessage(props) {
   return (
     <div className='warning-txt'>
       <p>Please correct input using the following formats: <br />
-      <span>CITYNAME</span> - <span>OTHERCITYNAME</span><br />
-      cities from <span>CITYNAME</span> in <span>#</span> jumps<br />
-      can I teleport from <span>CITYNAME</span> to <span>OTHERCITYNAME</span><br />
-      loop possible from <span>CITYNAME</span><br /><br />
+      <span>CITYX</span> - <span>CITYZ</span><br />
+      cities from <span>CITYX</span> in <span>#</span> jumps<br />
+      can I teleport from <span>CITYX</span> to <span>CITYZ</span><br />
+      loop possible from <span>CITYX</span><br /><br />
       error line was [{props.errorLine}]
       </p>
     </div>
